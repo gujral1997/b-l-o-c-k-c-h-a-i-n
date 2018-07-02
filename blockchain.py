617b7162a1946fdf154ca952c2089eeeba3bd1bf
a <anshgujral1997@gmail.com>
2018-07-02 18:36:25 -0400

    errors fixed

--- blockchain.py
+++ blockchain.py
@@ -12,48 +12,48 @@ class Blockchain:
         self.chain = []
         self.create_block(proof = 1, previous_hash = '0')
 
-        def create_block(self, proof, previous_hash):
-            block = {'index': len(self.chain)+1,
-                    'timestamp': str(datetime.datetime.now()),
-                    'proof': proof,
-                    'previous_hash': previous_hash
-            }
-            self.chain.append(block)
-            return block
+    def create_block(self, proof, previous_hash):
+        block = {'index': len(self.chain) + 1,
+                'timestamp': str(datetime.datetime.now()),
+                'proof': proof,
+                'previous_hash': previous_hash
+        }
+        self.chain.append(block)
+        return block
 
-        def get_previous_block(self):
-            return self.chain[-1]
-        
-        def proof_of_work(self, previous_proof):
-            new_proof = 1
-            check_proof = False
-            while check_proof is False:
-                hash_operation = hashlib.sha256(str(new_proof**2 - previous_proof**2).encode()).hexdigest()
-                if hash_operation[:4] == '0000':
-                    check_proof = True
-                else:
-                    new_proof += 1
-                return new_proof
-        
-        def hash(self, block):
-            encoded_block = json.dumps(block, sort_keys = True).encode()
-            return hashlib.sha256(encoded_block).hexdigest()
+    def get_previous_block(self):
+        return self.chain[-1]
+    
+    def proof_of_work(self, previous_proof):
+        new_proof = 1
+        check_proof = False
+        while check_proof is False:
+            hash_operation = hashlib.sha256(str(new_proof**2 - previous_proof**2).encode()).hexdigest()
+            if hash_operation[:4] == '0000':
+                check_proof = True
+            else:
+                new_proof += 1
+            return new_proof
+    
+    def hash(self, block):
+        encoded_block = json.dumps(block, sort_keys = True).encode()
+        return hashlib.sha256(encoded_block).hexdigest()
 
-        def is_chain_valid(self, chain):
-            previous_block = chain[0]
-            block_index = 1
-            while block_index < len(chain):
-                block = chain[block_index]
-                if block['previous_hash'] != self.hash(previous_block):
-                    return False
-                previous_proof = previous_block['proof']
-                proof = block['proof']
-                hash_operation = hashlib.sha256(str(proof**2 - previous_proof**2).encode()).hexdigest()
-                if hash_operation[:4] != '0000':
-                    return False
-                previous_block = block
-                block_index += 1
-            return True
+    def is_chain_valid(self, chain):
+        previous_block = chain[0]
+        block_index = 1
+        while block_index < len(chain):
+            block = chain[block_index]
+            if block['previous_hash'] != self.hash(previous_block):
+                return False
+            previous_proof = previous_block['proof']
+            proof = block['proof']
+            hash_operation = hashlib.sha256(str(proof**2 - previous_proof**2).encode()).hexdigest()
+            if hash_operation[:4] != '0000':
+                return False
+            previous_block = block
+            block_index += 1
+        return True
 
 # Mining Created Blockchain
 
@@ -73,7 +73,7 @@ def mine_block():
     previous_proof = previous_block['proof']
     proof = blockchain.proof_of_work(previous_proof)
     previous_hash = blockchain.hash(previous_block)
-    blockchain.create_block(proof, previous_hash)
+    block = blockchain.create_block(proof, previous_hash)
     response = {'message': 'Congrats, Block has been mined',
                 'index': block['index'],
                 'timestamp': block['timestamp'],
