59d5224f0332f40a29004b86ed96f2501216ef9f
a <anshgujral1997@gmail.com>
2018-07-02 16:08:16 -0400

    added basic blockchain code

---
+++ blockchain.py
@@ -0,0 +1,24 @@
+import datetime
+import hashlib
+import json
+from flask import Flask, jsonify
+
+# Block chain code
+
+class Blockchain:
+    def __init__(self):
+        self.chain = []
+        self.create_block(proof = 1, previous_hash = '0')
+
+        def create_block(self, proof, previous_hash):
+            block = {'index': len(self.chain)+1,
+                    'timestamp': str(datetime.datetime.now()),
+                    'proof': proof,
+                    'previous_hash': previous_hash
+            }
+            self.chain(block)
+            return block
+
+        def get_previous_block(self):
+            return self.chain[-1]
+
